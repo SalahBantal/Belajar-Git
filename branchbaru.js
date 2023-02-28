04ab42ae2b1baf802ebabe4c92f7d2551cb0e7d5
Muhammad Akbar <akubaru0@gmail.com>
2023-02-28 17:23:42 +0700

    menambahkan beberapa update untuk mencoba pertamakali git push

--- branchbaru.js
+++ branchbaru.js
@@ -1,2 +1,4 @@
 console.log("ini adalah file baru di branch terbaru");
 console.log("mari kita coba");
+console.log("kita coba lagi revertnya");
+
--- fungsi2.js
+++ fungsi2.js
@@ -1,3 +1,7 @@
 console.log ("hello bro");
 console.log ("apa kabar");
 console.log ("aku baik-baik saja sih");
+console.log ("bagaimana kabarmu");
+console.log ("semoga baik-baik saja");
+console.log ("oh iya aku ingin menayakan sesuatu");
+console.log ("bagaimana menurutmu tentang rasa?")
